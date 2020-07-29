913f7fe5947ade56226187f8bf77bf648d8aa762
uttamuk <uttamuk003@gmail.com>
2020-07-29 19:50:08 +0530

    Updates

---
+++ precache-manifest.7950faad669469c2e300bab9c4238b9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec243a2ad649028abea6ccc5f4e56c7e",
+    "url": "/AudioPlayer/index.html"
+  },
+  {
+    "revision": "eef707c1ec5bef2a1b14",
+    "url": "/AudioPlayer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "40c5028cad96eeb1e83e",
+    "url": "/AudioPlayer/static/js/2.0962cc46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AudioPlayer/static/js/2.0962cc46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef707c1ec5bef2a1b14",
+    "url": "/AudioPlayer/static/js/main.058d5449.chunk.js"
+  },
+  {
+    "revision": "8588a790117b8d49375d",
+    "url": "/AudioPlayer/static/js/runtime-main.25c7b6f8.js"
+  }
+]);
